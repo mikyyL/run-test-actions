0a0615cd8251f5df414cec06d1fe3110c73f28dd
artem <mikyedit@gmail.com>
2025-01-29 13:34:27 +0300

    push tests

---
+++ tests/test_static.py
@@ -0,0 +1,16 @@
+import allure
+
+
+@allure.feature('test numbers')
+class TestNumbers:
+    @allure.story('Тест номер 1')
+    def test_number_one(self):
+        with allure.step('Шаг 1 подготовка данных'):
+            x = 3
+            assert x == 3
+
+    @allure.story('Тест номер 2')
+    def test_number_two(self):
+        with allure.step('Шаг 1 подготовка данных'):
+            x = 3
+            assert x == 2
